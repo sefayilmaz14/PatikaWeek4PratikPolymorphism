48fe5398208571102e35205929aeb1fb0f26cf46
sefayilmaz <sefaylmaz14@gmail.com>
2024-08-09 21:50:27 +0300

    Add project files.

---
+++ PatikaWeek4PratikPolymorphism/BaseGeometricShape.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace PatikaWeek4PratikPolymorphism
+{
+    public class BaseGeometricShape // Ortak sınfımızı oluşturduk
+    {
+        //Propetry tanımlamaları
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        //Alan hesaplama methodu türetilen sınıflardada kullanılacağı için virtual olarak işlendi
+        public virtual int AreaCalculation()
+        {
+            return Height * Width;
+
+        }
+
+
+    }
+
+    //Kare alan hesaplama methodu ,override işlemi ve konsol bilgi mesajı
+    public class Square : BaseGeometricShape
+    {
+
+
+        public override int AreaCalculation()
+        {
+
+            int area = base.AreaCalculation();
+            Console.WriteLine($"Karenin Alanı:{area}");
+            return area;
+
+        }
+
+    }
+    //Dikdörtgen alan hesaplama methodu ,override işlemi ve konsol bilgi mesajı
+    public class Rectangle : BaseGeometricShape
+    {
+        public override int AreaCalculation()
+        {
+
+            int area = base.AreaCalculation();
+            Console.WriteLine($"Dikdörtgenin Alanı:{area}");
+            return area;
+
+        }
+    }
+    //Dikçgen alan hesaplama methodu ,override işlemi ve konsol bilgi mesajı
+    public class Triangle : BaseGeometricShape
+    {
+        public override int AreaCalculation()
+        {
+
+            int area = base.AreaCalculation() / 2;
+            Console.WriteLine($"Diküçgenin Alanı:{area}");
+            return area;
+
+        }
+    }
+}
---
+++ PatikaWeek4PratikPolymorphism/Program.cs
@@ -0,0 +1,10 @@
+﻿using PatikaWeek4PratikPolymorphism;
+//Kare Nesnesi oluşturma ve alan hesaplama
+BaseGeometricShape square = new Square { Height=10 , Width = 5};
+square.AreaCalculation();
+//Dikdörtgen Nesnesi oluşturma ve alan hesaplama
+BaseGeometricShape rectangle = new Rectangle { Height=20 , Width = 10 };
+rectangle.AreaCalculation();
+//Diküçgen Nesnesi oluşturma ve alan hesaplama
+BaseGeometricShape triangle = new Triangle { Height=10 , Width = 5 };
+triangle.AreaCalculation();
